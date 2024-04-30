02eb3c51fb3906cdb3063ee03ec9a101e2a6d10e
Krashiy <dimitrykravchenko2000@gmail.com>
2024-04-30 11:12:18 +0200

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  base: "/MELANISH/",
+  base: "MELANISH/",
   plugins: [
     ViteImageOptimizer({
       png: {
